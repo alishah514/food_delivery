f99d305c9bf94a170c0ee2cd5cf061bae7367f02
alishah514 <ali83_shah@yahoo.com>
2022-05-20 23:42:08 +0500

    5 hours 53 mins completed

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,11 @@ import 'package:food_delivery/pages/food/recommended_food_detail.dart';
 import 'package:food_delivery/pages/home/food_page_body.dart';
 import 'package:food_delivery/pages/home/main_food_page.dart';
 import 'package:get/get.dart';
+import 'helper/dependencies.dart' as dep;
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dep.init();
   runApp(const MyApp());
 }
 
